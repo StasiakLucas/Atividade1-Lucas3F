43a8934bf9e3c58a0734a991fa881f95e4638a9c
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-08-05 22:52:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Lucas Stasiak";
+const idade = 28;
+const altura = 1.85;
+const massa = 
+const cidade
+
+console.log(meuNome);
+console.log(idade);
+
+// adicione nesse programa, altura, massa e cidade onde mora
+
